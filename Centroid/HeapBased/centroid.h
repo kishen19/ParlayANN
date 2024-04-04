71128b121369589d5f1348a354dea7e81b998b41
kishen19 <kishen19@umd.edu>
2024-04-04 13:37:26 -0400

    update

--- Centroid/HeapBased/centroid.h
+++ Centroid/HeapBased/centroid.h
@@ -62,19 +62,23 @@ void Centroid(GraphType &Graph, long k, BuildParams &BP,
   // Init Priority Queue
   using kv = std::tuple<distanceType,indexType,indexType>;
   auto G = Graph.Get_Graph_Read_Only();
-  QueryParams QP((long) 1, BP.L,  (double) 0.0, (long) Points.size(), (long) G.max_degree());
+  QueryParams QP((long) 2, BP.L,  (double) 0.0, (long) Points.size(), (long) G.max_degree());
   auto h = parlay::sequence<kv>::from_function(n,[&](size_t i){
     auto out = beam_search(Points[i], G, Points, start_point, QP);
     if (i<10){
       std::cout << i << " " << out.first.first.size() << std::endl;
     }
-    return std::make_tuple(out.first.first[0].second,i,out.first.first[0].first);
+    if (out.first.first[0].first!=i){
+      return std::make_tuple(out.first.first[0].second,i,out.first.first[0].first);
+    } else {
+      return std::make_tuple(out.first.first[1].second,i,out.first.first[1].first);
+    }
   });
   Graph.Release_Graph(std::move(G));
   for (int i=0; i<10; i++){
     std::cout << std::get<0>(h[i]) << " " << std::get<1>(h[i]) << " " << std::get<2>(h[i]) << std::endl;
   }
-  std::priority_queue<kv> H(h.begin(), h.end());
+  std::priority_queue<kv, vector<kv>, std::greater<kv>> H(h.begin(), h.end());
   std::cout << "Heap Init Done" << std::endl;
 
   // Centroid Process
@@ -83,15 +87,12 @@ void Centroid(GraphType &Graph, long k, BuildParams &BP,
   H.pop();
   auto u = std::get<1>(cand);
   auto v = std::get<2>(cand);
-  std::cout << "u: " << u << ", v: " << v << ", dist" << std::get<0>(cand) << std::endl;
+  std::cout << "u: " << u << ", v: " << v << ", dist: " << std::get<0>(cand) << std::endl;
   auto p1 = Points[u];
   auto p2 = Points[v];
   auto c = p1.centroid(p2,n);
   std::cout << "Computed Centroid" << std::endl;
   Points.add_point(c);
-  parlay::sequence<indexType> inserts = {(indexType) n};
-  I.insert(Graph, Points, BuildStats, inserts);
-  std::cout << "Inserted Centroid" << std::endl;
   parlay::sequence<indexType> deletes = {u,v};
   std::cout << "Starting Deletion" << std::endl;
   I.lazy_delete(deletes);
@@ -99,6 +100,9 @@ void Centroid(GraphType &Graph, long k, BuildParams &BP,
   I.consolidate(Graph, Points);
   I.end_delete_epoch(Graph);
   std::cout << "Finished deleting" << std::endl;
+  parlay::sequence<indexType> inserts = {(indexType) n};
+  I.insert(Graph, Points, BuildStats, inserts);
+  std::cout << "Inserted Centroid" << std::endl;
   n++;
 
   // size_t DEL_THRESH = n/20;
